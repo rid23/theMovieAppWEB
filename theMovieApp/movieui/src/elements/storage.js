c91b8e8a85deaac99eef5e01da6ee2828c3deb8f
rohan37-pro <rohan2002maji@gmail.com>
2023-03-14 23:50:32 +0530

    working on redux storage

---
+++ theMovieApp/movieui/src/elements/storage.js
@@ -0,0 +1,22 @@
+import {  configureStore, legacy_createStore as createStore } from "redux"
+
+
+
+// storage architecture
+// {
+//     movie : "",
+//     duration : "",
+//     casts : {
+//         "castName" : {
+//             gender : "",
+//             character : "",
+//             Dialogues : {
+//                 "dialogue" : {
+//                     from : "",
+//                     To : ""
+//                 }
+//             }
+//         }
+//     }
+// }
+
